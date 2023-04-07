c4ee0936d995f533b3df5d0d6d1ffa6adc0c3085
Vlad <thatvlad@icloud.com>
2023-04-07 14:27:15 +0200

    Struct

--- qspi/qspi.cpp
+++ qspi/qspi.cpp
@@ -4,37 +4,47 @@ using namespace daisy;
 
 DaisySeed hw;
 
-const size_t param_count = 2;
-float param_ram[param_count];
-float DSY_QSPI_BSS qspi_buffer[param_count];
+struct PersistentParams {
+	int even_pattern_a;
+	int cword_pattern_a;
+	int even_pattern_b;
+	int cword_pattern_b; 
+};
 
-#define READONLY
+PersistentParams params_ram;
+PersistentParams DSY_QSPI_BSS params_qspi;
+
+//#define READONLY
 
 int main(void)
 {
 	hw.Configure();
 	hw.Init();
-	
-#ifdef READONLY
 	hw.StartLog();
-#else
-	param_ram[0] = 0.65;
-	param_ram[1] = 0.75;
 
-	size_t size = sizeof(param_ram[0]) * param_count;
+#ifndef READONLY
+	params_ram.cword_pattern_a = 1;
+	params_ram.cword_pattern_b = 3;
+	params_ram.even_pattern_a = 5;
+	params_ram.even_pattern_b = 7;
+
+	size_t size = sizeof(params_ram);
 	/** Grab physical address from pointer */
-	size_t address = (size_t)qspi_buffer;
+	
+	size_t address = (size_t)(&params_qspi);
 	/** Erase qspi and then write that wave */
 	hw.qspi.Erase(address, address + size);
-	hw.qspi.Write(address, size, (uint8_t*)param_ram);
+	hw.qspi.Write(address, size, (uint8_t*)(&params_ram));
 #endif
 
 	while(1) {
 #ifdef READONLY
-	auto param_a = static_cast<int>(qspi_buffer[0] * 1000);
-	auto param_b = static_cast<int>(qspi_buffer[1] * 1000);
+	auto even_pattern_a = params_qspi.even_pattern_a;
+	auto even_pattern_b = params_qspi.even_pattern_b;
+	auto cword_pattern_a = params_qspi.cword_pattern_a;
+	auto cword_pattern_b = params_qspi.cword_pattern_b;
 
-	hw.PrintLine("%d %d", param_a, param_b);
+	hw.PrintLine("%d %d %d %d", even_pattern_a, even_pattern_b, cword_pattern_a, cword_pattern_b);
 
 	System::Delay(1000);
 #endif
